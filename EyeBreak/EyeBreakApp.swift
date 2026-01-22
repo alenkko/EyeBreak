8d85d8d688864e32d453f56f710dfb6d67ecbf81
alenjurina <alen.jurina@fer.hr>
2026-01-22 12:50:16 +0100

    Initial Commit

---
+++ EyeBreak/EyeBreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeBreakApp.swift
+//  EyeBreak
+//
+//  Created by Alen Jurina on 22.01.2026..
+//
+
+import SwiftUI
+
+@main
+struct EyeBreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
